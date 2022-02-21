20b11a0d7e29888768649705c51e13d3eb87286c
JunnoTo <80600972+JunnoTo@users.noreply.github.com>
2022-02-21 13:25:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http')
 
 http.createServer(function(req, res){
     res.writeHead(200, {"Content-type": "text/plain"})
-    res.end("Hello world! it is I, Topias\n")
+    res.end("Hello world!\n")
 }).listen(process.env.PORT)
